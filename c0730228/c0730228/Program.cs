565c4d2f81707eeee4f89f52e8261c3b9b392299
Arshdeep  Brar <730228@student.tor.loc>
2019-03-29 21:15:03 -0400

    nfdf

--- c0730228/c0730228/Program.cs
+++ c0730228/c0730228/Program.cs
@@ -18,22 +18,18 @@ namespace c0730228
         static async void Download()
         {
             await Network.Download();
-            Console.WriteLine("Download Completed");
+            Console.WriteLine("Download Complete");
         }
 
     }
 
     class Network
     {
-        static public Task Download()
+        public async void Download()
         {
-<<<<<<< HEAD
-            return Task.Run(() => Thread.Sleep(3000));
-
-=======
-            Thread.Sleep(66000);
-            Console.WriteLine("Download complete...");
->>>>>>> 18bb1ec3dfaa1436b26648bee0a98ff2ab344602
+            HttpClient client = new HttpClient();
+            var data = await client.GetStringAsync("https://torontopubliclibrary.ca");
+            Console.WriteLine(data);
         }
     }
 }
